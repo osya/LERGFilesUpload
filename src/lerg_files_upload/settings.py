56b83a1177176fc1cbe219de806235b689bdb136
Valeriy Osipov <info@vosipov.com>
2017-09-06 09:07:21 +0300

    Add PRESERVE_CONTEXT_ON_EXCEPTION

--- src/lerg_files_upload/settings.py
+++ src/lerg_files_upload/settings.py
@@ -51,3 +51,4 @@ class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
     BCRYPT_LOG_ROUNDS = 4  # For faster tests; needs at least 4 to avoid "ValueError: Invalid rounds"
     WTF_CSRF_ENABLED = False  # Allows form testing
+    PRESERVE_CONTEXT_ON_EXCEPTION = False
